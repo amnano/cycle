f72d834dda0cd77658ae4a1856025dfda0a654bb
amnano <molto.a@gmail.com>
2024-12-12 18:16:17 +0100

    vercel and article deletion

--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -1,20 +1,20 @@
-type YouTubeProps = {
-  id: string
-  title?: string
-}
+import React from 'react'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
-const YouTube = ({ id, title = 'YouTube Video' }: YouTubeProps) => {
+const YouTube = ({ id, title }: { id: string; title: string }) => {
   return (
-    <div className="relative overflow-hidden w-full" style={{ paddingBottom: '56.25%' }}>
-      <iframe
-        src={`https://www.youtube.com/embed/${id}`}
-        title={title}
-        className="absolute top-0 left-0 w-full h-full"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+    <div className="w-full overflow-hidden">
+      <div className="relative">
+        <LiteYouTubeEmbed
+          id={id}
+          title={title}
+          wrapperClass="relative left-0 top-0 h-full w-full"
+          playerClass="absolute top-0 left-0"
+        />
+      </div>
     </div>
   )
 }
 
-export default YouTube 
+export default YouTube
